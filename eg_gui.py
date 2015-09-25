0a8f911430bae64210cee6494f56181836c6d469
PLR <51248199+plredmond@users.noreply.github.com>
2015-09-24 17:44:47 -0700

    allow clicking and then moving the reticle around before releasing the mouse; add options for reticle color; add image-sized secondary reticle

--- eg_gui.py
+++ eg_gui.py
@@ -28,13 +28,16 @@ events = \
 
 class MouseQuery(object):
 
-    def __init__(self, window, image, count=None, size=None, annot=None):
+    def __init__(self, window, image, count=None, size=None, annot=None, reticle_color=None, big_reticle_color=None):
         self.__w = window
         self.__src = image
         self.__dst = numpy.empty_like(image)
         self.__ct = 1 if count is None else count
         self.__s = 4 if size is None else size
         self.__an = (lambda im, pts: None) if annot is None else annot
+        self.__c1 = (0, 0, 255) if reticle_color is None else reticle_color
+        self.__c2 = big_reticle_color
+        self.__d = False
         self.__xy = (0, 0)
         self.__pts = []
 
@@ -49,17 +52,29 @@ class MouseQuery(object):
         if events[event] == 'EVENT_MOUSEMOVE':
             self.__xy = (x, y)
         elif events[event] == 'EVENT_LBUTTONDOWN':
+            self.__d = True
+        elif events[event] == 'EVENT_LBUTTONUP' and self.__d:
+            self.__d = False
             self.__pts.append((x, y))
         else:
             print('Ignoring:', events[event], x, y, flags, param)
 
     def _loop(self):
         while len(self.__pts) < self.__ct:
+            # blank
             numpy.copyto(self.__dst, self.__src)
+            # user annotation
             self.__an(self.__dst, self.__xy, self.__pts)
+            # reticle
             x, y = self.__xy
-            cv2.line(self.__dst, (x - self.__s, y), (x + self.__s, y), (0, 0, 255))
-            cv2.line(self.__dst, (x, y - self.__s), (x, y + self.__s), (0, 0, 255))
+            if self.__c2 and self.__d:
+                ym, xm = self.__dst.shape[:2]
+                cv2.line(self.__dst, (0, y), (xm, y), self.__c2)
+                cv2.line(self.__dst, (x, 0), (x, ym), self.__c2)
+            if self.__c1:
+                cv2.line(self.__dst, (x - self.__s, y), (x + self.__s, y), self.__c1)
+                cv2.line(self.__dst, (x, y - self.__s), (x, y + self.__s), self.__c1)
+            # display
             cv2.imshow(w, self.__dst)
             if cv2.waitKey(1) == 27:
                 raise ValueError('ESC key')
@@ -76,7 +91,7 @@ if __name__ == '__main__':
         assert 0 <= c < 255
         if pts:
             cv2.rectangle(dst, pts[0], xy, [c] * 3)
-    with MouseQuery(w, first, count=2, size=20, annot=a) as loop:
+    with MouseQuery(w, first, count=2, size=20, annot=a, big_reticle_color=(0,255,255)) as loop:
         box = loop()
 
     for frame in itertools.chain([first], m):
