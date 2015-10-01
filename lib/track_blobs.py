2eaf197104f0d3f1f4ddbcb8974af079f9b7cd10
PLR <51248199+plredmond@users.noreply.github.com>
2015-10-01 13:17:59 -0700

    replace KeyPoint with CV2KeyPoint and add BlobPoint to contain it; update calling code

--- lib/track_blobs.py
+++ lib/track_blobs.py
@@ -20,15 +20,17 @@ import lib.cviter as cviter
 import lib.circles as circles
 import lib.blob_params as blob_params
 
-KeyPoint = cv2.KeyPoint().__class__
+CV2KeyPoint = cv2.KeyPoint().__class__
+BlobPoint = collections.namedtuple('BlobPoint', 'pt keypoint frameinfo')
 FlowPoint = collections.namedtuple('FlowPoint', 'pt status error')
 
-def new_path(point):
-    assert isinstance(point, KeyPoint)
-    return [point]
+def new_path(fi, kp):
+    assert type(fi).__name__ == 'FrameInfo'
+    assert isinstance(kp, CV2KeyPoint)
+    return [BlobPoint(pt=kp.pt, keypoint=kp, frameinfo=fi)]
 
 def path_is_active(path):
-    assert isinstance(path[0], (KeyPoint, FlowPoint))
+    assert isinstance(path[0], (BlobPoint, FlowPoint))
     latest = path[-1]
     if isinstance(latest, FlowPoint):
         return latest.status == 1
@@ -36,7 +38,7 @@ def path_is_active(path):
         return True
 
 def path_loc(path):
-    assert isinstance(path[0], (KeyPoint, FlowPoint))
+    assert isinstance(path[0], (BlobPoint, FlowPoint))
     assert path_is_active(path)
     loc = path[-1].pt
     assert len(loc) == 2
@@ -52,34 +54,35 @@ def flow_path(path, pt_status_err, max_err=100):
             if status == 1 and err < max_err else \
             path
 
-def anchor_path(path, point):
+def anchor_path(path, fi, kp):
     '''return a new path with the point at the end'''
-    assert isinstance(point, KeyPoint)
-    return path[:] + [point]
+    assert type(fi).__name__ == 'FrameInfo'
+    assert isinstance(kp, CV2KeyPoint)
+    return path[:] + [BlobPoint(pt=kp.pt, keypoint=kp, frameinfo=fi)]
 
-def new_path_group(detect, ims):
-    (bim0, _), (_, _) = ims
-    return [new_path(kp) for kp in detect(bim0)]
+def new_path_group(detect, ti):
+    (m0, _), (bim0, _), (_, _) = ti
+    return [new_path(m0, kp) for kp in detect(bim0)]
 
-def flow_path_group(pg, ims):
+def flow_path_group(pg, ti):
     '''find updated locations for each path head by optical flow
        return
         [(ndarray<2>, 0|1, float)] # for each path, indicate flow-loc, status, and error
     '''
-    (_, _), (fim0, fim1) = ims
+    (_, _), (_, _), (fim0, fim1) = ti
     fs1 = cv2.calcOpticalFlowPyrLK(fim0, fim1,
             numpy.array([path_loc(path) for path in pg], numpy.float32))
     return [(point, status, error)
             for point, (status,), (error,) in itertools.izip(*fs1)]
 
-def anchor_path_group(pg, detect, ims, match_dist=100):
+def anchor_path_group(pg, detect, ti, match_dist=100):
     '''match newly detected blobs against path heads (closer than match_dist)
        return
-        ( [KeyPoint|None] # for each path, indicating the best blob or no match
-        , [KeyPoint] # for each unmatched blob
+        ( [CV2KeyPoint|None] # for each path, indicating the best blob or no match
+        , [CV2KeyPoint] # for each unmatched blob
         )
     '''
-    (_, bim1), (_, _) = ims
+    (_, _), (_, bim1), (_, _) = ti
 
     # detect new blobs
     bs1 = detect(bim1)
@@ -106,7 +109,7 @@ def anchor_path_group(pg, detect, ims, match_dist=100):
                     itertools.islice(ranking, match_count))
 
     # gale-shapley
-    path_match = {} # {P: KeyPoint}
+    path_match = {} # {P: CV2KeyPoint}
     blob_match = {} # {B: P}
     def match(P, B):
         # remove an existing match, if any
@@ -146,18 +149,19 @@ def annot_hist(im, path_group):
                 iterutils.slidingWindow(2, path))
 
 def annot_point(im, point):
-    if isinstance(point, KeyPoint):
-        cv2.drawKeypoints(im, [point], im, (25,125,0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    assert isinstance(point, (BlobPoint, FlowPoint))
+    if isinstance(point, BlobPoint):
+        cv2.drawKeypoints(im, [point.keypoint], im, (25,125,0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
     else:
         cv2.circle(im, tuple(point.pt), point.error, (50,50,255))
 
 def annot_segment(im, points):
     p0, p1 = points
     color = \
-        { (KeyPoint, KeyPoint): (0, 0, 0) # black
+        { (BlobPoint, BlobPoint): (0, 0, 0) # black
         , (FlowPoint, FlowPoint): (50,50,255) # deep red
-        , (KeyPoint, FlowPoint): (0,255,255) # light yellow
-        , (FlowPoint, KeyPoint): (25,125,0) # deep green
+        , (BlobPoint, FlowPoint): (0,255,255) # light yellow
+        , (FlowPoint, BlobPoint): (25,125,0) # deep green
         }[type(p0), type(p1)]
     cv2.line(im, tuple(map(int, p0.pt)), tuple(map(int, p1.pt)), color)
 
@@ -170,8 +174,8 @@ in_circle = lambda x_y_r, pt, dist=euclidean_dist: dist(x_y_r[:2], pt) <= x_y_r[
 def gen_flagger(half_petri_x_y_r):
     # flaggers :: {str: (Path -> bool)}
     flaggers = \
-        { 'path is comprised of less than 10% KeyPoints'
-        : lambda path: len(filter(lambda p: isinstance(p, KeyPoint), path)) / len(path) < 0.1
+        { 'path is comprised of less than 10% BlobPoint'
+        : lambda path: len(filter(lambda p: isinstance(p, BlobPoint), path)) / len(path) < 0.1
         , 'path is completely outside of the inner half-radius of the petri dish'
         # TODO: change to manhattan dist
         : lambda path: all(not in_circle(half_petri_x_y_r, p.pt) for p in path)
@@ -182,6 +186,9 @@ def gen_flagger(half_petri_x_y_r):
             if f(path):
                 return s
     return flagger
+
+# TODO: wrap CV2KeyPoint & associated FrameInfo in a BlobPoint where detection occurs
+# TODO: wrap (pt, status, err) & associated FrameInfo in a FlowPoint where optical-flow occurs
 
 def trackBlobs \
         ( detector
@@ -232,29 +239,28 @@ def trackBlobs \
         )
 
     # loop on joined input streams
-    for meta, ims in itertools.izip(metaInput, itertools.izip(blobInput, flowInput)):
-        # TODO: get rid of the annotation code because it requires destructuring ims
-        (bim0, bim1), (fim0, fim1) = ims
+    for ti in itertools.izip(metaInput, blobInput, flowInput):
+        (m0, m1), (bim0, bim1), (fim0, fim1) = ti
         # allocate
         if ns is None:
             ns = TrackState \
-                ( paths = new_path_group(detector.detect, ims)
+                ( paths = new_path_group(detector.detect, ti)
                 , annotCur = numpy.empty(bim1.shape[:2] + (3,), bim1.dtype)
                 , annotHist = numpy.empty(bim1.shape[:2] + (3,), bim1.dtype)
                 )
         # flow (update path heads according to how pixels have moved)
         ns = ns._replace(paths = map \
             ( functools.partial(flow_path, max_err=max_flow_err)
-            , ns.paths, flow_path_group(ns.paths, ims)
+            , ns.paths, flow_path_group(ns.paths, ti)
             ))
         # anchor (redetect blobs and match them against paths)
-        anchors, blobs = anchor_path_group(ns.paths, detector.detect, ims,
+        anchors, blobs = anchor_path_group(ns.paths, detector.detect, ti,
                 match_dist=anchor_match_dist)
         assert len(anchors) == len(ns.paths)
         ns = ns._replace(paths \
-            = [anchor_path(p, a) if a else p
+            = [anchor_path(p, m1, a) if a else p
                 for p, a in zip(ns.paths, anchors)]
-            + [new_path(b) for b in blobs]
+            + [new_path(m1, b) for b in blobs]
             )
 
         # paths get a "flow" every frame (where status is 1 and error is less than max_flow_err)
